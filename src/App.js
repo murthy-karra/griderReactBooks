4dd29d557dad09d8750080ca0d1f26cd3c903426
Murthy Karra <karrad@karra.co>
2023-04-26 14:17:11 -0700

    Part 1

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
 import { useState } from "react";
 import BookCreate from "./components/BookCreate";
-
+import BookList from "./components/BookList";
 function App() {
   const [books, setBooks] = useState([]);
 
   const createBook = (title) => {
-    console.log("Need to add book with title ", title);
+    //console.log("Need to add book with title ", title);
+    const id = Math.round(Math.random() * 9999);
+    console.log(id, title);
+    setBooks([...books, { id, title }]);
+  };
+
+  const deleteBookById = (id) => {
+    const updatedBooks = books.filter((book) => {
+      return book.id !== id;
+    });
+    setBooks(updatedBooks);
+  };
+
+  const editBookById = (id, newTitle) => {
+    const updatedBooks = books.map((book) => {
+      if (book.id === id) {
+        return { id: book.id, title: newTitle };
+      }
+      return book;
+    });
+    setBooks(updatedBooks);
   };
 
   return (
-    <div>
+    <div className="app">
+      <h1> Reading List </h1>
+      <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
       <BookCreate onCreate={createBook} />
     </div>
   );
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -8,16 +8,18 @@ function BookCreate({ onCreate }) {
   };
 
   const handleSubmit = (event) => {
+    //console.dir(event.target);
     event.preventDefault();
     onCreate(title);
     setTitle("");
   };
   return (
-    <div>
+    <div className="book-create">
+      <h3>Add a Book</h3>
       <form onSubmit={handleSubmit}>
         <label>Title</label>
-        <input value={title} onChange={handleChange} />
-        <button>Create!</button>
+        <input className="input" value={title} onChange={handleChange} />
+        <button className="button">Create!</button>
       </form>
     </div>
   );
