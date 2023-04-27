2433ff6f5908c89cc7165276b2955e52a26ea400
Murthy Karra <karrad@karra.co>
2023-04-27 15:14:57 -0700

    Finished and Completed App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
+
 function App() {
   const [books, setBooks] = useState([]);
 
-  const createBook = (title) => {
-    //console.log("Need to add book with title ", title);
-    const id = Math.round(Math.random() * 9999);
-    console.log(id, title);
-    setBooks([...books, { id, title }]);
+  const fetchBooks = async () => {
+    const response = await axios.get("http://localhost:3001/books");
+    setBooks(response.data);
+  };
+  useEffect(() => {
+    fetchBooks();
+    console.log("useEffect with empty array as 2nd arg called");
+  }, []);
+
+  useEffect(() => {
+    console.log("useEffect with no 2nd arg called");
+  });
+
+  const createBook = async (title) => {
+    const response = await axios.post("http://localhost:3001/books", {
+      title,
+    });
+
+    setBooks([...books, response.data]);
   };
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`);
+
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const editBookById = (id, newTitle) => {
+  const editBookById = async (id, newTitle) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title: newTitle,
+    });
+
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { id: book.id, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
